78f23ed5faa10b18426feda3129cfed002f191ff
Michael Chi <chicong@outlook.com>
2023-04-02 22:03:12 +0800

    Nirvana

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,3 @@
-use crate::config::CONFIG;
-
-mod app;
-mod cache;
-mod config;
-mod data;
-mod download;
-mod error;
-mod gui;
-mod info;
-mod log;
-mod scan;
-mod sync;
-mod update;
-mod util;
-mod version;
-
 fn main() {
-    // for init config
-    let _ = &CONFIG.log_level;
-    // config::init_config();
-
-    log::init_log();
-
-    app::start();
+    println!("Hello!")
 }
